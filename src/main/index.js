87e57cd18a36a5499e4cc2842cb0af2fb05c6347
chulphan <chkim100617@gmail.com>
2021-01-12 11:17:44 +0900

    format 함수의 이름을 usd 함수로 변경하여 함수의 핵심을 더 잘 드러내도록 했고, 인자에서 단위를 변경하지 않고 함수내에서 단위 변환도 할 수 있도록 처리하였다

--- src/main/index.js
+++ src/main/index.js
@@ -4,10 +4,15 @@ const plays = require('../data/plays.json');
 /**
  * 임시 변수는 자신이 속한 루틴에만 의미가 있어서 루틴이 길고 복잡해지기 쉽다
  * 본래 fotmat 함수는 임시 변수에 함수를 대입한 형태인데, 저자는 직접 선언해서 사용하도록 바꾼다고 한다
+ *
+ * format 이라는 이름은 정확히 무엇을 하는지 정확히 표현하지 못한다
+ * 이 함수의 핵심은 화폐 단위 맞추기이므로 함수 선언 바꾸기를 적용했다
+ *
+ * 긴 함수를 작게 쪼개는 리팩터링은 이름을 잘 지어야만 효과가 있고, 이름이 좋으면 함수 본문을 읽지 않고도 무슨 일을 할 수 있는지 알 수 있다...
  */
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
-    {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber);
+    {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber / 100);
 }
 
 // 간단히 perf 를 전달하는 것으로 포인트 계산이 가능해진다
@@ -62,12 +67,12 @@ function statement(invoice, plays) {
 
     // 청구 내역을 출력한다
     //                                        amountFor(perf) 를 사용하여 thisAmount 변수를 인라인한다
-    result += `    ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    result += `    ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf); // thisAmount 변수를 인라인한다
   }
 
   // 임시 변수였던 format 을 함수 호출로 대체했다
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
