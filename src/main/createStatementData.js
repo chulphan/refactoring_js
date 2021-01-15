ab37f9c8a5dd48901316445529b1b936cb770753
chulphan <chkim100617@gmail.com>
2021-01-15 12:59:27 +0900

    희극에 대한 공연료 계산도 오버라이드 하여 처리하였다

--- src/main/createStatementData.js
+++ src/main/createStatementData.js
@@ -70,7 +70,15 @@ class TragedyCalculator extends PerformanceCalculator {
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
 
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
