b056d1279d89886d1f371420fe1632a23ef285ad
chulphan <chkim100617@gmail.com>
2021-01-15 12:57:42 +0900

    비극 계산 서브 클래스에 비극에 대한 공연료 계산 로직을 생성했고, 슈퍼클래스에서 비극 계산을 시도하면 에러를 던지도록 유도했다(이 부분은 명시적으로 비극 계산 서브 클래스를 사용하게 하기 위함)

--- src/main/createStatementData.js
+++ src/main/createStatementData.js
@@ -31,11 +31,7 @@ class PerformanceCalculator {
 
     switch (this.play.type) { // play를 playFor() 호출로 변경
       case "tragedy": //비극
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
+        throw 'Error occurred';
       case "comedy": //희극
         result = 30000;
         if (this.performance.audience > 20) {
@@ -63,7 +59,14 @@ class PerformanceCalculator {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
 
+    return result;
+  }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
